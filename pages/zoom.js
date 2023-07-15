8ad9d3dab6a7df5ddda4d5dfd3fe87a911f17254
H4rsh4nk <pathakharsh13@gmail.com>
2023-07-14 23:37:54 -0400

    Updating comments in zoom.js 	modified:   pages/zoom.js

--- pages/zoom.js
+++ pages/zoom.js
@@ -1,20 +1,19 @@
-import { useEffect, useState } from 'react';
-import router from 'next/router';
-const axios = require('axios');
-const { ZOOM } = require('../Constants/Zoom')
+import { useEffect, useState } from "react";
+import router from "next/router";
+const axios = require("axios");
+const { ZOOM } = require("../Constants/Zoom");
 
 const Meeting = () => {
-    
-    //Chatbox component
-    const [message, setMessage] = useState('');
-    const [displayedMessage, setDisplayedMessage] = useState('');
-    const [isMessageVisible, setIsMessageVisible] = useState(false);
+    //-------------- Chatbox component ----------------------
+    const [message, setMessage] = useState(""); 
+    const [displayedMessage, setDisplayedMessage] = useState(""); 
+    const [isMessageVisible, setIsMessageVisible] = useState(false); 
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        setDisplayedMessage(message);
-        setMessage('');
-        setIsMessageVisible(true);
+        setDisplayedMessage(message); 
+        setMessage(""); 
+        setIsMessageVisible(true); 
     };
 
     const handleChange = (e) => {
@@ -22,92 +21,113 @@ const Meeting = () => {
     };
 
     const handleCloseMessage = () => {
-        setIsMessageVisible(false);  
+        setIsMessageVisible(false);
     };
+    
 
-    //Zoom SDK
+    //-------------- Zoom WEB SDK setup ----------------------
     useEffect(() => {
         return async () => {
             new Promise(async (resolve, reject) => {
-                const ZoomEmbed = await (await import('@zoomus/websdk/embedded')).default;
+                const ZoomEmbed = await (
+                    await import("@zoomus/websdk/embedded")
+                ).default;
 
                 resolve(ZoomEmbed.createClient());
-            }).then(async (client) => {
-                let meetingSDKElement = document.getElementById('meetingSDKElement');
+            })
+                .then(async (client) => {
+                    let meetingSDKElement = document.getElementById("meetingSDKElement");
 
-                client.init({
-                    language: 'en-US',
-                    zoomAppRoot: meetingSDKElement,
-                    enforceVirtualBackground: true,
-                    customize: {
-                        video: {
-                            viewSizes: {
-                                default: {
-                                    height: 200,
-                                    width: 300
+                    // Initialize the client and cutomize the UI for zoom meeting
+                    client.init({
+                        language: "en-US",
+                        zoomAppRoot: meetingSDKElement,
+                        enforceVirtualBackground: true,
+                        customize: {
+                            video: {
+                                viewSizes: {
+                                    default: {
+                                        height: 200,
+                                        width: 300,
+                                    },
+                                    ribbon: {
+                                        width: 400,
+                                    },
                                 },
-                                ribbon: {
-                                    width: 400
-                                }
-                            }
+                            },
+                            chat: {
+                                popper: {
+                                    placement: "right",
+                                },
+                            },
                         },
-                        chat: {
-                            popper: {
-                                placement: 'right',
-                            }
-                        }
-                    }
-                });
+                    });
 
-                
+                    //Get credientials from index.js in URL
+                    let payload = router.query;
 
-                let payload = router.query;
-                const { data } = await axios({
-                    url: '/api/Zoom/',
-                    method: 'POST',
-                    data: payload
-                }).then(response => {
-                    return response;
-                }).catch(error => {
-                    console.log('---signature axios request error ---', error);
-                });
+                    // Get the signature from the server (/pages/api/Zoom/index.js)
+                    const { data } = await axios({
+                        url: "/api/Zoom/",
+                        method: "POST",
+                        data: payload,
+                    })
+                        .then((response) => {
+                            return response;
+                        })
+                        .catch((error) => {
+                            console.log("---signature axios request error ---", error);
+                        });
 
-                client.join({
-                    signature: data.signature,
-                    sdkKey: data.sdkKey,
-                    meetingNumber: payload.meetingNumber,
-                    password: payload.passWord,
-                    userName: payload.userName,
-                    userEmail: '',
-                    tk: '',
-                    zak: ''
+                    // Join the meeting as guest user
+                    client.join({
+                        signature: data.signature,
+                        sdkKey: data.sdkKey,
+                        meetingNumber: payload.meetingNumber,
+                        password: payload.passWord,
+                        userName: payload.userName,
+                        userEmail: "",
+                        tk: "",
+                        zak: "",
+                    });
                 })
-
-            }).catch(error => {
-                console.log('---Error inside useEffect---', error);
-            });
-
-            
-        }
+                .catch((error) => {
+                    console.log("---Error inside useEffect---", error);
+                });
+        };
     });
 
     return (
         <div>
+            {/* Displaying chatbox message at top of the screen */}
             {isMessageVisible && (
                 <div className="flex justify-center" id="messageContainer">
                     <div className="bg-green-500 text-white p-2 rounded-lg mb-2">
                         <div className="flex items-center">
                             <span className="mr-2">{displayedMessage}</span>
-                            
+
                             <button>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6" onClick={handleCloseMessage}>
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                            </svg>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth={1.5}
+                                    stroke="currentColor"
+                                    className="w-6 h-6"
+                                    onClick={handleCloseMessage}
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M6 18L18 6M6 6l12 12"
+                                    />
+                                </svg>
                             </button>
                         </div>
                     </div>
                 </div>
             )}
+            {/* Meeting Component and Chatbox Component */}
             <div className="flex">
                 <div className="w-70 flex-auto" id="meetingSDKElement"></div>
                 <div className="w-30 flex justify-end items-end">
@@ -129,10 +149,9 @@ const Meeting = () => {
                 </div>
             </div>
         </div>
+    );
+};
 
-    )
-}
-
-Meeting.displayName = 'Zoom Component View';
+Meeting.displayName = "Zoom Component View";
 
 export default Meeting;
