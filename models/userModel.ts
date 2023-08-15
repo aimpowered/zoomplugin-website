90ac14e5bb9197a5db802b215763b53da692b1cb
H4rsh4nk <pathakharsh13@gmail.com>
2023-08-15 01:24:33 -0400

    Fixing bycrypt typo, mongoose import and try catch

--- models/userModel.ts
+++ models/userModel.ts
@@ -1,8 +1,7 @@
 // Creating Schema to save user data in mongoDB
 // Compare passwords, hash and store
-import { Model, models, model } from "mongoose";
-import { Document, Schema } from "mongoose";
-import bcrupt from "bcrypt";
+import { Model, models, model, Document, Schema } from "mongoose";
+import bcrypt from "bcrypt";
 
 interface UserDocument extends Document {
     email: string;
@@ -26,22 +25,14 @@ const userSchema = new Schema<UserDocument, {}, Methods>({
 //Hash the password before saving
 userSchema.pre("save", async function (next) {
     if (!this.isModified("password")) return next();
-    try {
-        const salt = await bcrupt.genSalt(10);
-        this.password = await bcrupt.hash(this.password, salt);
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
         next();
-    } catch (err) {
-        throw(err);
-    }   
 });
 
 //Compare password method
 userSchema.methods.comparePassword = async function (password) {
-    try {
-        return await bcrupt.compare(password, this.password);
-    } catch (err) {
-        throw(err);
-    }
+        return await bcrypt.compare(password, this.password);
 };
 
 //Create User model if it doesn't exist
