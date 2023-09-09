255a298433439ca38484fc844dcf61fba49e26a1
H4rsh4nk <pathakharsh13@gmail.com>
2023-09-09 16:39:50 -0400

    Updated AI model detection functionality

--- app/(private_route)/ai-model/page.js
+++ app/(private_route)/ai-model/page.js
@@ -1,10 +1,17 @@
 "use client"
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 export default function YourComponent() {
   const webcamRef = useRef(null);
   const labelContainerRef = useRef(null);
+  const [showAlert, setShowAlert] = useState(false);
 
+  function displayAlert() {
+    setShowAlert(true);
+    setTimeout(() => {
+      setShowAlert(false);
+    }, 5000);
+  }
   useEffect(() => {
     async function initTeachableMachine() {
       const URL = "https://teachablemachine.withgoogle.com/models/RFPmylbbW/";
@@ -49,28 +56,60 @@ export default function YourComponent() {
             window.requestAnimationFrame(loop);
           }
 
-          async function predict() {
-            const prediction = await model.predict(webcam.canvas);
-            for (let i = 0; i < maxPredictions; i++) {
-              const classPrediction =
-                prediction[i].className +
-                ": " +
-                prediction[i].probability.toFixed(2);
-              labelContainer.childNodes[i].innerHTML = classPrediction;
-            }
-          }
+         async function predict() {
+           const prediction = await model.predict(webcam.canvas);
+
+           // Find the index of the "thumbs up" class
+           const thumbsUpIndex = prediction.findIndex(
+             (p) => p.className === "Thumbs up"
+           );
+
+           // Find the index of the "thumbs down" class
+           const thumbsDownIndex = prediction.findIndex(
+             (p) => p.className === "Thumbs down"
+           );
+
+           if (thumbsUpIndex !== -1) {
+             const thumbsUpProbability = prediction[thumbsUpIndex].probability;
+
+             if (thumbsUpProbability > 0.5 && !showAlert) {
+               console.log("Thumbs up detected");
+               displayAlert();
+             }
+           }
+
+           if (thumbsDownIndex !== -1) {
+             const thumbsDownProbability =
+               prediction[thumbsDownIndex].probability;
+
+             if (thumbsDownProbability > 0.5 && !showAlert) {
+               console.log("Thumbs down detected");
+               displayAlert();
+             }
+           }
+
+           for (let i = 0; i < maxPredictions; i++) {
+             const classPrediction =
+               prediction[i].className +
+               ": " +
+               prediction[i].probability.toFixed(2);
+             labelContainer.childNodes[i].innerHTML = classPrediction;
+           }
+         }
         };
       };
     }
-
     initTeachableMachine();
   }, []);
 
   return (
     <div className="flex flex-col justify-center items-center">
-      <div className="flex">Teachable Machine Image Model for thumbs up and down</div>
+      <div className="flex">
+        Teachable Machine Image Model for thumbs up and down
+      </div>
       <div className="flex" id="webcam-container" ref={webcamRef}></div>
       <div className="flex" id="label-container" ref={labelContainerRef}></div>
+      <div>{showAlert && <div className="alert">Gesture Detected!</div>}</div>
     </div>
   );
 }
