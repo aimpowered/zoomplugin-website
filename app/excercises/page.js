5ff218a1f77756bbb722d576fb8a94b8c4f57779
samikpal <samikpal0@gmail.com>
2023-10-30 11:48:23 -0400

    added videos to load from the harddrive

--- app/excercises/page.js
+++ app/excercises/page.js
@@ -1,34 +1,28 @@
 "use client";
 import React from "react";
-// import { Icon } from "@iconify/react";
+import { Icon } from "@iconify/react";
 
-function YouTubeVideo({ videoId, title, description, time }) {
+function LocalVideo({ videoSource, title, description, time }) {
   const boxStyles = {
     backgroundColor: "#FEBD2F",
   };
+
   return (
     <div className="w-full md:w-1/2 p-2">
       <div
         className=" rounded-3xl shadow-lg shadow-gray-500 p-4"
         style={boxStyles}
       >
-        <iframe
-          width="100%"
-          height="350px"
-          src={`https://www.youtube.com/embed/${videoId}`}
-          allowFullScreen
-        ></iframe>
+        <video width="100%" height="350" controls>
+          <source src={videoSource} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
         <div className="h-24">
           <h2 className="text-xl font-bold mt-4">{title}</h2>
           <p className="text-base mt-2">{description}</p>
         </div>
         <div className="flex flex-nowrap gap-2">
-          {/* <Icon
-            icon="ion:time-outline"
-            width="20px"
-            height="20px"
-            style={{ marginTop: "3px" }}
-          /> */}
+          <Icon icon="ion:time-outline" width="20px" height="20px" />
           <p className="text-base ">{time}</p>
         </div>
       </div>
@@ -57,14 +51,14 @@ export default function App() {
           meeting
         </div>
         <div className="flex flex-nowrap gap-20">
-          <YouTubeVideo
-            videoId="Nhdl70HABYQ?si=h3OGQDEcPufUEAJD"
+          <LocalVideo
+            videoSource="/videos/1 Minute Breathing Exercise_ Mini Meditation _ Simple Breathing Exercise _ One Minute Breathe Bubble.mp4"
             title={title1}
             description={desc1}
             time="1 min"
           />
-          <YouTubeVideo
-            videoId="VIDEO_ID_2"
+          <LocalVideo
+            videoSource="/videos/Standing Upper Body Stretches.mp4"
             title={title2}
             description={desc2}
             time="2 min"
