0f58175423213ac6a294db3c8035364ba0faa849
Harshank <pathakharsh13@gmail.com>
2023-10-31 19:32:33 -0400

    Moving exercise page to private route

--- app/(private_route)/profile/page.js
+++ app/(private_route)/profile/page.js
@@ -12,10 +12,17 @@ function Profile() {
                     &lt; Home
                 </a>
             </div>
-            
+
             <div className="flex flex-wrap justify-center mx-auto">
                 <div className="w-full md:w-1/2 lg:w-1/3 p-4">
-                    <div className="border rounded-3xl shadow-lg shadow-gray-500 p-4 ml-20 mr-20" style={{ width: "225px", height: "225px", backgroundColor: "#FEBD2F"}}>
+                    <div
+                        className="border rounded-3xl shadow-lg shadow-gray-500 p-4 ml-20 mr-20"
+                        style={{
+                            width: "225px",
+                            height: "225px",
+                            backgroundColor: "#FEBD2F",
+                        }}
+                    >
                         <a href="/badge">
                             <div className="flex flex-col items-center font-bold mb-4">
                                 <RibbonOutline
@@ -24,19 +31,38 @@ function Profile() {
                                     width="60px"
                                     className="mt-1 mr-2"
                                 />
-                                <span style={{ fontSize: "14px", textAlign: "center"}}>Badge</span>
+                                <span
+                                    style={{
+                                        fontSize: "14px",
+                                        textAlign: "center",
+                                    }}
+                                >
+                                    Badge
+                                </span>
                             </div>
                             <div className="ml-4 mr-4 my-4">
-                                <div style={{ fontSize: "14px", textAlign: "center"}}>
+                                <div
+                                    style={{
+                                        fontSize: "14px",
+                                        textAlign: "center",
+                                    }}
+                                >
                                     Share what makes you uniquely you
                                 </div>
                             </div>
                         </a>
-                    </div> 
-                </div>     
-                    
+                    </div>
+                </div>
+
                 <div className="w-full md:w-1/2 lg:w-1/3 p-4">
-                    <div className="border rounded-3xl shadow-lg shadow-gray-500 p-4 ml-20 mr-20" style={{ width: "225px", height: "225px", backgroundColor: "#FEBD2F"}}>
+                    <div
+                        className="border rounded-3xl shadow-lg shadow-gray-500 p-4 ml-20 mr-20"
+                        style={{
+                            width: "225px",
+                            height: "225px",
+                            backgroundColor: "#FEBD2F",
+                        }}
+                    >
                         <a href="/bar-chart-outline">
                             <div className="flex flex-col items-center font-bold mb-4">
                                 <BarChartOutline
@@ -45,19 +71,38 @@ function Profile() {
                                     width="60px"
                                     className="mt-1 mr-2"
                                 />
-                                <span style={{ fontSize: "14px", textAlign: "center"}}>Post-Meeting Summaries</span>
+                                <span
+                                    style={{
+                                        fontSize: "14px",
+                                        textAlign: "center",
+                                    }}
+                                >
+                                    Post-Meeting Summaries
+                                </span>
                             </div>
                             <div className="ml-4 mr-4 my-4">
-                                <div style={{ fontSize: "14px", textAlign: "center"}}>
+                                <div
+                                    style={{
+                                        fontSize: "14px",
+                                        textAlign: "center",
+                                    }}
+                                >
                                     Access past meeting summaries and insights
                                 </div>
                             </div>
                         </a>
-                    </div> 
+                    </div>
                 </div>
 
                 <div className="w-full md:w-1/2 lg:w-1/3 p-4">
-                    <div className="border rounded-3xl shadow-lg shadow-gray-500 p-4 ml-20 mr-20" style={{ width: "225px", height: "225px", backgroundColor: "#FEBD2F"}}>
+                    <div
+                        className="border rounded-3xl shadow-lg shadow-gray-500 p-4 ml-20 mr-20"
+                        style={{
+                            width: "225px",
+                            height: "225px",
+                            backgroundColor: "#FEBD2F",
+                        }}
+                    >
                         <a href="/journal">
                             <div className="flex flex-col items-center font-bold mb-4">
                                 <BookOutline
@@ -66,19 +111,38 @@ function Profile() {
                                     width="60px"
                                     className="mt-1 mr-2"
                                 />
-                                <span style={{ fontSize: "14px", textAlign: "center"}}>Journal</span>
+                                <span
+                                    style={{
+                                        fontSize: "14px",
+                                        textAlign: "center",
+                                    }}
+                                >
+                                    Journal
+                                </span>
                             </div>
                             <div className="ml-4 mr-4 my-4">
-                                <div style={{ fontSize: "14px", textAlign: "center"}}>
+                                <div
+                                    style={{
+                                        fontSize: "14px",
+                                        textAlign: "center",
+                                    }}
+                                >
                                     Review past thoughts or create new ones
                                 </div>
                             </div>
                         </a>
-                    </div> 
+                    </div>
                 </div>
-                
+
                 <div className="w-full md:w-1/2 lg:w-1/3 p-4">
-                    <div className="border rounded-3xl shadow-lg shadow-gray-500 p-4 ml-20 mr-20" style={{ width: "225px", height: "225px", backgroundColor: "#FEBD2F"}}>
+                    <div
+                        className="border rounded-3xl shadow-lg shadow-gray-500 p-4 ml-20 mr-20"
+                        style={{
+                            width: "225px",
+                            height: "225px",
+                            backgroundColor: "#FEBD2F",
+                        }}
+                    >
                         <a href="/difficult-word-bank">
                             <div className="flex flex-col items-center font-bold mb-4">
                                 <BookOutline
@@ -87,20 +151,40 @@ function Profile() {
                                     width="60px"
                                     className="mt-1 mr-2"
                                 />
-                                <span style={{ fontSize: "14px", textAlign: "center"}}>Difficult Word Bank</span>
+                                <span
+                                    style={{
+                                        fontSize: "14px",
+                                        textAlign: "center",
+                                    }}
+                                >
+                                    Difficult Word Bank
+                                </span>
                             </div>
                             <div className="ml-4 mr-4 my-4">
-                                <div style={{ fontSize: "14px", textAlign: "center"}}>
-                                    Identify challenging words and expand your word bank
+                                <div
+                                    style={{
+                                        fontSize: "14px",
+                                        textAlign: "center",
+                                    }}
+                                >
+                                    Identify challenging words and expand your
+                                    word bank
                                 </div>
                             </div>
                         </a>
-                    </div> 
+                    </div>
                 </div>
 
                 <div className="w-full md:w-1/2 lg:w-1/3 p-4">
-                    <div className="border rounded-3xl shadow-lg shadow-gray-500 p-4 ml-20 mr-20" style={{ width: "225px", height: "225px", backgroundColor: "#FEBD2F"}}>
-                        <a href="/relaxation-exercise">
+                    <div
+                        className="border rounded-3xl shadow-lg shadow-gray-500 p-4 ml-20 mr-20"
+                        style={{
+                            width: "225px",
+                            height: "225px",
+                            backgroundColor: "#FEBD2F",
+                        }}
+                    >
+                        <a href="/exercise">
                             <div className="flex flex-col items-center font-bold mb-4">
                                 <BookOutline
                                     color={"#35377D"}
@@ -108,17 +192,28 @@ function Profile() {
                                     width="60px"
                                     className="mt-1 mr-2"
                                 />
-                                <span style={{ fontSize: "14px", textAlign: "center"}}>Relaxation Exercise</span>
+                                <span
+                                    style={{
+                                        fontSize: "14px",
+                                        textAlign: "center",
+                                    }}
+                                >
+                                    Relaxation Exercise
+                                </span>
                             </div>
                             <div className="ml-4 mr-4 my-4">
-                                <div style={{ fontSize: "14px", textAlign: "center"}}>
+                                <div
+                                    style={{
+                                        fontSize: "14px",
+                                        textAlign: "center",
+                                    }}
+                                >
                                     Customize meeting relaxation activities
                                 </div>
                             </div>
                         </a>
-                    </div> 
+                    </div>
                 </div>
-
             </div>
         </div>
     );
